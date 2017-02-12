ab75dc1fcdebe0c9d6047e4144be82770d47eeed
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-12 13:35:00 -0600

    Added overloads for sum,mean,mode to work on floats and doubles.

--- stats.net/Summary.cs
+++ stats.net/Summary.cs
@@ -5,10 +5,78 @@ namespace Stats.Net
 {
     public static class Summary {
         public static int Sum(IEnumerable<int> vector){
-            return vector.Aggregate((a,b) => a+b);
+            if(vector.Any()){
+                return vector.Aggregate((a,b) => a+b);
+            }
+            return default(int);
         }
-        public static double Mean(IEnumerable<int> vector){
-            return (double)Sum(vector) / vector.Count();
+
+        public static float Sum(IEnumerable<float> vector){
+            if(vector.Any()){
+                return vector.Aggregate((a,b) => a+b);
+            }
+            return default(float);
         }
+
+        public static double Sum(IEnumerable<double> vector){
+            if(vector.Any()){
+                return vector.Aggregate((a,b) => a+b);
+            }
+            return default(double);
+        }
+
+        public static double? Mean(IEnumerable<int> vector){
+            if(vector.Any()){
+                return (double)Sum(vector) / vector.Count();
+            }
+            return null;
+        }
+
+        public static double? Mean(IEnumerable<float> vector){
+            if(vector.Any()){
+                return (double)Sum(vector) / vector.Count();
+            }
+            return null;
+        }
+
+         public static double? Mean(IEnumerable<double> vector){
+            if(vector.Any()){
+                return (double)Sum(vector) / vector.Count();
+            }
+            return null;
+        }
+
+        private static IDictionary<T, int> CountOccurences<T>(IEnumerable<T> vector){
+            IDictionary<T,int> values = new Dictionary<T, int>();
+            foreach(var item in vector){
+                if(values.Keys.Contains(item)){
+                    values[item]++;
+                }
+                else {
+                    values.Add(item, 1);
+                }
+            }
+            return values;
+        }
+
+        public static IEnumerable<int> Mode(IEnumerable<int> vector){
+            var valueOccurences = CountOccurences(vector);
+            var max = valueOccurences.Select(v => v.Value).Max();
+            return max == 1 ? null : valueOccurences.Where(v => v.Value == max).Select(v => v.Key);
+        }
+
+        public static IEnumerable<float> Mode(IEnumerable<float> vector){
+            var valueOccurences = CountOccurences(vector);
+            var max = valueOccurences.Select(v => v.Value).Max();
+            return max == 1 ? null : valueOccurences.Where(v => v.Value == max).Select(v => v.Key);
+        }
+
+        public static IEnumerable<double> Mode(IEnumerable<double> vector){
+            var valueOccurences = CountOccurences(vector);
+            var max = valueOccurences.Select(v => v.Value).Max();
+            return max == 1 ? null : valueOccurences.Where(v => v.Value == max).Select(v => v.Key);
+        }
+
+
     }
 }
--- tests/stats.net.tests/SummaryTests.cs
+++ tests/stats.net.tests/SummaryTests.cs
@@ -1,23 +1,155 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 using Stats.Net;
+using System.Linq;
 using static Stats.Net.Summary;
 
-namespace Stats.Net.Tests {
-    
-    public class SummaryTests {
+namespace Stats.Net.Tests
+{
+
+    public class SummaryTests
+    {
 
         [Fact]
-        public void Summary_Sum_Test(){
-            int[] data = new int[] {1,2,3,4};
+        public void Sum_GivenAnArrayOfInts_FindsTheCorrectSum()
+        {
+            int[] data = new int[] { 1, 2, 3, 4 };
             var sum = Sum(data);
-            Assert.Equal(10,sum);
+            Assert.Equal(10, sum);
         }
 
         [Fact]
-        public void Summary_Mean_Test(){
-            int[] data = new int[]{1,2,3,4};
+        public void Sum_GivenAnArrayOfFloats_FindsTheCorrectSum()
+        {
+            float[] data = new float[] { 1.7f, 2.2f, 3.9f, 4.3f };
+            var sum = Sum(data);
+            Assert.Equal(12.1, sum, 1);
+        }
+
+        [Fact]
+        public void Sum_GivenAnArrayOfDoubles_FindsTheCorrectSum()
+        {
+            double[] data = new double[] { 1.7, 2.2, 3.9, 4.3 };
+            var sum = Sum(data);
+            Assert.Equal(12.1, sum, 1);
+        }
+
+
+        [Fact]
+        public void Sum_GivenANullArray_FindsTheCorrectSum()
+        {
+            int[] data = null;
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var sum = Sum(data);
+            });
+        }
+
+        [Fact]
+        public void Sum_GivenAnEmptyArray_ReturnsZero()
+        {
+            int[] data = new int[0];
+            var sum = Sum(data);
+            Assert.Equal(0, sum);
+        }
+
+        [Fact]
+        public void Mean_GivenAnArrayOfInts_ReturnsTheAverage()
+        {
+            int[] data = new int[] { 1, 2, 3, 4 };
             var mean = Mean(data);
             Assert.Equal(2.5, mean);
+        }
+
+        [Fact]
+        public void Mean_GivenAnArrayOfFloats_ReturnsTheAverage()
+        {
+            float[] data = new float[] { 1.5f, 2.25f, 3.25f, 4.5f };
+            var mean = Mean(data);
+            Assert.Equal(2.875f, mean);
+        }
+
+        [Fact]
+        public void Mean_GivenAnArrayOfDoubles_ReturnsTheAverage()
+        {
+            double[] data = new double[] { 1.5, 2.25, 3.25, 4.5 };
+            var mean = Mean(data);
+            Assert.Equal(2.875, mean);
+        }
+
+        [Fact]
+        public void Mean_GivenAnEmptyList_ReturnsNull()
+        {
+            int[] data = new int[0];
+            var mean = Mean(data);
+            Assert.Null(mean);
+        }
+
+        [Fact]
+        public void Mode_GivenAnArrayOfIntsWithASingleMode_ReturnsAListWithASingleElement()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5, 5, 6, 7 };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 1);
+            Assert.True(mode.Contains(5));
+        }
+
+        [Fact]
+        public void Mode_GivenAnArrayOfFloatsWithASingleMode_ReturnsAListWithASingleElement()
+        {
+            float[] data = new float[] { 1.1f, 2.2f, 3.3f, 4.4f, 5.5f, 5.5f, 6.6f, 7.7f };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 1);
+            Assert.True(mode.Contains(5.5f));
+        }
+
+        [Fact]
+        public void Mode_GivenAnArrayOfDoublesWithASingleMode_ReturnsAListWithASingleElement()
+        {
+            double[] data = new double[] { 1.1, 2.2, 3.3, 4.4, 5.5, 5.5, 6.6, 7.7 };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 1);
+            Assert.True(mode.Contains(5.5));
+        }
+
+
+        [Fact]
+        public void Mode_GivenAArrayOfIntsWithMultipleModes_ReturnsAListWithASingleElement()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 4, 5, 5, 6, 7 };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 2);
+            Assert.True(mode.Contains(4));
+            Assert.True(mode.Contains(5));
+        }
+
+        [Fact]
+        public void Mode_GivenAArrayOfFloatsWithMultipleModes_ReturnsAListWithASingleElement()
+        {
+            float[] data = new float[] { 1.1f, 2.2f, 3.3f, 4.4f, 4.4f, 5.5f, 5.5f, 6.6f, 7.7f };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 2);
+            Assert.True(mode.Contains(4.4f));
+            Assert.True(mode.Contains(5.5f));
+        }
+
+        [Fact]
+        public void Mode_GivenAArrayOfDoublesWithMultipleModes_ReturnsAListWithASingleElement()
+        {
+            double[] data = new double[] { 1.1, 2.2, 3.3, 4.4, 4.4, 5.5, 5.5, 6.6, 7.7 };
+            var mode = Mode(data);
+            Assert.Equal(mode.Count(), 2);
+            Assert.True(mode.Contains(4.4));
+            Assert.True(mode.Contains(5.5));
+        }
+
+        [Fact]
+        public void Mode_GivenAListOfNumbersWithNoMode_ReturnsNull()
+        {
+            int[] data = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            var mode = Mode(data);
+            Assert.Null(mode);
         }
     }
 
