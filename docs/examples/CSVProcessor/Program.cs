3f2573d70e1ac3ae01a39da546bc8886a4c398cc
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-20 21:38:04 -0600

    Including an example console app in the docs.

---
+++ docs/examples/CSVProcessor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DataIO.Read;
+using DataIO.Formats;
+using StatsNet;
+using System.IO;
+
+namespace CSVProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var reader = Reader.OpenFile<CsvFormat>(@"C:\Users\Taylor Hutchison\Source\Repos\stats.net\tests\data\csvsample1.csv");
+
+                var header = reader.GetHeader();
+                foreach (var item in header)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
