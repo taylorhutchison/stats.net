d7efb5c419189ba4169bc34acf6aeedaf879d9a4
Taylor Hutchison <seanthutchison@gmail.com>
2017-02-22 13:02:44 -0600

    Change path to test data to be relative.

--- docs/examples/CSVProcessor/Program.cs
+++ docs/examples/CSVProcessor/Program.cs
@@ -3,28 +3,37 @@ using DataIO.Read;
 using DataIO.Formats;
 using StatsNet;
 using System.IO;
+using System.Linq;
 
 namespace CSVProcessor
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             try
             {
-                var reader = Reader.OpenFile<CsvFormat>(@"C:\Users\Taylor Hutchison\Source\Repos\stats.net\tests\data\csvsample1.csv");
-
+                var reader = Reader.OpenFile<CsvFormat>(@"..\..\..\tests\data\csvsample1.csv");
                 var header = reader.GetHeader();
                 foreach (var item in header)
                 {
                     Console.WriteLine(item);
                 }
+
+                var col4 = reader.GetColumn<int>(3);
+                col4.ToList().ForEach(Console.WriteLine);
+
+                var range = Summary.Range(col4);
+
+                Console.WriteLine($"Range: {range.First()} - {range.Last()}");
+
             }
             catch (ArgumentException ex)
             {
                 Console.WriteLine(ex.Message);
-                Console.ReadKey();
             }
+
+            Console.ReadKey();
 
         }
     }
